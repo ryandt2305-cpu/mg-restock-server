906acaebc3f5cd0212fdd9c142a799b9113cc0e6
ryandt2305-cpu <ryandt2305@gmail.com>
2026-02-03 17:56:07 +1000

    polish

--- scripts/set-import-meta.mjs
+++ scripts/set-import-meta.mjs
@@ -5,5 +5,5 @@ const META_FILE = path.join(process.cwd(), "data", "meta.json");
 const meta = JSON.parse(fs.readFileSync(META_FILE, "utf8"));
 meta.importedAt = 1769944415316;
 meta.importSource = "discord-html";
-meta.importFile = "Magic Circle - ?? Magic Garden - ping [1392142706964303933].html";
+meta.importFile = "Magic Circle - 🍄 Magic Garden - ping [1392142706964303933].html";
 fs.writeFileSync(META_FILE, JSON.stringify(meta, null, 2) + "\n", "utf8");
